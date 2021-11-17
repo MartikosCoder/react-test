3ddacba4d3d928099b74a11e5339ec4d88c435b1
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2021-11-17 15:57:35 +0300

    Переработан хедер проекта

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
+import Logo from "../../assets/logo.svg";
 
 import { useNavigate } from "react-router-dom";
 
 function Header() {
-  const [is_home_active] = useState(window.location.pathname === '/');
-  const [is_show_active] = useState(window.location.pathname === '/show');
+  const [is_home_active, setIsHomeActive] = useState("");
+  const [is_show_active, setIsShowActive] = useState("");
+
+  useEffect(() => {
+    setIsHomeActive(window.location.pathname === "/");
+    setIsShowActive(window.location.pathname === "/show");
+  });
 
   const navigate = useNavigate();
 
   const goTo = (e, path) => {
     e.preventDefault();
 
-    navigate(path, {replace: true});
-  }
+    navigate(path, { replace: true });
+  };
 
   return (
     <header>
-        <a href="/" onClick={e => goTo(e, '/')} className={is_home_active ? "active" : ""}>Главная</a>
-        <a href="/show" onClick={e => goTo(e, '/show')} className={is_show_active ? "active" : ""}>График</a>
+      <div className="logo">
+        <img src={Logo} alt="Alef" />
+      </div>
+      <nav>
+        <a
+          href="/"
+          onClick={(e) => goTo(e, "/")}
+          className={is_home_active ? "active" : ""}
+        >
+          Форма
+        </a>
+        <a
+          href="/show"
+          onClick={(e) => goTo(e, "/show")}
+          className={is_show_active ? "active" : ""}
+        >
+          Превью
+        </a>
+      </nav>
     </header>
   );
 }
