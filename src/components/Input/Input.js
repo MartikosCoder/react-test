e912b2e66f90e3c9302ec4e8dacf068c3683630d
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2021-11-18 17:30:08 +0300

    Правки

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./Input.css";
 
 function Input(props) {
+  const changeAction = e => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, '');
+    props.setValue(e.target.value);
+  }
+
   return (
     <div className="form__input">
-      <input className={String(props.value).length > 0 ? "entered" : ""} type={props.type} value={props.value} onChange={e => props.setValue(e.target.value)}/>
+      <input className={String(props.value).length > 0 ? "entered" : ""} type={props.type} value={props.value} onChange={changeAction}/>
       <label>{props.label}</label>
     </div>
   );
